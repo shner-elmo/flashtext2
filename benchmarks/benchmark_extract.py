5b9b107a4fa3339e71bb3e101048892b6b11bc09
Shneor <770elmo@gmail.com>
2023-08-13 16:53:54 +0200

    Updated benchmarks to show 4 lines

--- benchmarks/benchmark_extract.py
+++ benchmarks/benchmark_extract.py
@@ -1,86 +1,95 @@
 from __future__ import annotations
 
+import time
+import random
+import itertools
+from typing import TYPE_CHECKING, Iterator
+
 import flashtext
 import flashtext2
 import pandas as pd
 
-import time
-import random
-from typing import TYPE_CHECKING
-
-from utils import all_words  # , pretty_print
+from utils import all_words
 
 if TYPE_CHECKING:
     from flashtext2 import KeywordProcessor
 
+N_TESTS = 5
 
-def test() -> list[tuple]:
-    lst = []
-    # pretty_print('count', 'flashtext', 'flashtext2')
+
+# TODO; use regular text with human selected keywords.
+def benchmark() -> Iterator[dict[str, ...]]:
     for i in range(0, 100001, 1000):
+        data = {'count': i}
+        output = []
+
         words = random.sample(all_words, 10_000)
         sentence = ' '.join(words)  # len(story) == 10,000 * 6 = 60,000 chars
         keywords = random.sample(all_words, i)
 
-        flashtext2.KeywordProcessor.split_sentence('abc d ef')  # call the function to cache the regex compilation
+        # call this function to cache the regex compilation
+        flashtext2.KeywordProcessor.split_sentence('abc d ef')
         # ------ tests --------------------------------------------------------------------------------------
+
         kp = flashtext.KeywordProcessor()
         kp.add_keywords_from_list(keywords)
 
         start = time.perf_counter()
-        out1 = kp.extract_keywords(sentence, span_info=False)
-        time1 = time.perf_counter() - start
-        del kp  # use only flashtext 2.0 for the next tests
-
-        kp = flashtext2.KeywordProcessor()
-        kp.add_keywords_from_list(keywords)
+        out = kp.extract_keywords(sentence, span_info=True)
+        data['flashtext (with span-info)'] = time.perf_counter() - start
+        output.append(out)
 
         start = time.perf_counter()
-        out2 = kp.extract_keywords(sentence, span_info=False)
-        time2 = time.perf_counter() - start
+        out = kp.extract_keywords(sentence, span_info=False)
+        data['flashtext'] = time.perf_counter() - start
+        output.append(out)
         del kp
 
-        # print(time1, time2, time3)
-        # ---------------------------------------------------------------------------------------------------
+        kp2 = flashtext2.KeywordProcessor()
+        kp2.add_keywords_from_list(keywords)
 
-        # to make sure we exhausted the generators if the output is a list
-        assert isinstance(out1, list)
-        assert isinstance(out2, list)
-        # assert isinstance(out3, list)
-        assert len(out1) == len(out2), ('Length:', len(out1), len(out2), out1, out2)
-        assert out1 == out2
+        start = time.perf_counter()
+        out = kp2.extract_keywords(sentence, span_info=True)
+        data['flashtext2 (with span-info)'] = time.perf_counter() - start
+        output.append(out)
 
-        # you can uncomment this if you want copy and paste the output in gsheets or excel
-        # pretty_print(i, time1, time2)
-        lst.append((i, time1, time2))
-    return lst
+        start = time.perf_counter()
+        out = kp2.extract_keywords(sentence, span_info=False)
+        data['flashtext2'] = time.perf_counter() - start
+        output.append(out)
+        del kp2
+
+        # to make sure we exhausted the generators
+        for x in output:
+            assert isinstance(x, list), 'Iterator is not exhausted.'
+
+        # compare span_info True with True, and False with False.
+        assert output[0] == output[2]
+        assert output[1] == output[3]
+
+        yield data
 
 
 def main():
-    def mean(it) -> float:
-        return sum(it) / len(it)
+    data = itertools.chain(*(benchmark() for _ in range(N_TESTS)))
 
-    cols = [list(zip(*test())) for _ in range(5)]
-    n_cols = len(cols[0])
+    df = pd.DataFrame(data=data)
+    avg_df = df.groupby('count').mean()  # this is necessary if N_TESTS > 1
+    assert len(avg_df) == len(df) // N_TESTS
 
-    new_cols = [[] for _ in range(n_cols)]
-    for x in range(5):
-        for i in range(n_cols):
-            # print([cols[x][i]])
-            new_cols[i].append(cols[x][i])
-
-    # get the average of each column
-    new_cols = [list(map(mean, zip(*col_grp))) for col_grp in new_cols]
-
-    count, time1, time2 = new_cols
-    df = pd.DataFrame({
-        'count': count,
-        'flashtext': time1,
-        'flashtext 2.0': time2,
-        # 'flashtext 2.0 (beta)': time3,
-    })
-    plt = df.plot.line(title='Time For Extracting Keywords', x='count', xlabel='Word Count',
-                       ylabel='Seconds', y=['flashtext', 'flashtext 2.0'], grid=True)
+    name_color_map = {
+        'flashtext (with span-info)': '#1f77b4',
+        'flashtext': '#17becf',
+        'flashtext2 (with span-info)': '#ff7f0e',
+        'flashtext2': '#d62728',
+    }
+    plt = avg_df.plot.line(
+        title='Time For Extracting Keywords',
+        xlabel='Word Count',
+        ylabel='Seconds',
+        color=[name_color_map[col] for col in avg_df.columns],
+        grid=True,
+    )
     plt.figure.savefig('extract-keywords.png')
 
 
